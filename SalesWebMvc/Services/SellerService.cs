4e555f3d5b277145603fe9497ea0814a2a4d6ad4
ademirt <ademirt@gmail.com>
2020-10-05 22:34:28 -0300

    SellerService - Formulário de cadastro

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,5 +20,12 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        //método para inserir um novo vendedor...
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
