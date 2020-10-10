493c7b7fce86d27fd7d48c3025a0ec5b586360a0
ademirt <ademirt@gmail.com>
2020-10-10 13:34:18 -0300

    Ajustado Simple Search e Grouping Search.

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
 using System;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
+        //tamanho máximo / mínimo digitos
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "{0} deve possuir entre {2} a {1} caracteres!")]
         public string Name { get; set; }
 
         //Classe Department possui `N` Sellers....
